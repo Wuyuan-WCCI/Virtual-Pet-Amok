1b9515c73c7e201dd6931b46e2326955f2a01123
wuyuan-wcci <dranest01@gmail.com>
2023-07-23 16:43:24 -0400

    Checking

--- src/main/java/pets_amok/Application.java
+++ src/main/java/pets_amok/Application.java
@@ -1,4 +1,10 @@
 package pets_amok;
 
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the new project.");
+    }
+
+    public void getGreeting() {
+    }
 }
